d1ce8c25c7322a95096ff58149127161d0e24f46
pldrake26 <pldrake26@outlook.com>
2019-08-19 18:27:31 -0500

    Made the code pretty.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header.js';
-import GamesHeader from './GamesHeader.js';
-import Games from './Games.js';
-import Footer from './Footer.js';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header.js";
+import GamesHeader from "./GamesHeader.js";
+import Games from "./Games.js";
+import Footer from "./Footer.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
 class BamaSchedule extends Component {
   render() {
@@ -20,7 +20,4 @@ class BamaSchedule extends Component {
   }
 }
 
-ReactDOM.render(
-  <BamaSchedule />,
-  document.getElementById('root')
-);
+ReactDOM.render(<BamaSchedule />, document.getElementById("root"));
